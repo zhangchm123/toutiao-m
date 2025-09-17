26537683fef8e053ee0e5a4110176103b9ebe121
zhangchm <2697105880@qq.com>
2025-09-17 21:41:40 +0800

    '项目初始化-调整目录结构'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 3005,
+    open: true
+  },
+  lintOnSave: false
 })
